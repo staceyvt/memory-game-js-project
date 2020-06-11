639f083253f9d2648d4925454293042829e9ac71
staceyvt <staceythomas627@gmail.com>
2020-06-11 08:36:19 -0400

    memory-game-js-project

---
+++ js/app.js
@@ -0,0 +1,76 @@
+let clickedCard = null;
+let preventClick = false;
+let combosFound = 0;
+const colors = [
+    'pink',
+    'yellow',
+    'red',
+    'cyan',
+    'blue',
+    'teal',
+    'orange',
+    'green'
+];
+
+
+const cards = [...document.querySelectorAll('.card')];
+for(let color of colors){
+const cardAIndex = parseInt(Math.random() * cards.length);
+const cardA = cards[cardAIndex];
+cards.splice(cardAIndex,1);
+cardA.className += ` ${color}`;  //space must be included
+cardA.setAttribute('data-color', color);
+
+const cardBIndex = parseInt(Math.random() * cards.length);
+const cardB = cards[cardBIndex];
+cards.splice(cardBIndex,1);
+cardB.className += ` ${color}`;
+cardB.setAttribute('data-color', color);
+}
+
+function onCardClicked(e){
+    const target = e.currentTarget;
+    console.log(target.className);  //this provides details of each target/click
+
+
+if(
+    preventClick ||
+    target === clickedCard ||
+    target.className.includes('done')
+){
+    return;
+}
+
+target.className = target.className
+.replace ('color-hidden', '')
+.trim();
+target.className += ' done';  //there has to be a space in front of the word to avoid concatination
+
+if(!clickedCard){
+clickedCard = target; 
+}else if(clickedCard){
+ if(
+     clickedCard.getAttribute('data-color') !==
+     target.getAttribute('data-color')
+ ){
+    preventClick = true;
+    setTimeout(() => {
+    clickedCard.className =
+    clickedCard.className.replace('done', '').trim()+
+    ' color-hidden';
+    target.className=
+    target.className.replace('done', '').trim()+
+    ' color-hidden';
+    clickedCard = null;
+    preventClick = false;
+    },500);
+ }
+ else{
+     combosFound++;
+     clickedCard = null;
+     if(combosFound === 8){
+         alert("YOU WIN");
+     }
+ }
+}
+}
